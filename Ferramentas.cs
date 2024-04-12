5a6220e7eec5cd4169572e5661289f2acb47c28f
LUCASGOODPROGRAMMER <160272134+LUCASGOODPROGRAMMER@users.noreply.github.com>
2024-04-12 14:41:12 -0400

    Add files via upload

---
+++ Ferramentas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaCadastro
+{
+    public static class Ferramentas
+    {       
+        public static void Menu()
+        {
+            Console.Clear();            
+            Console.WriteLine("1 - CADASTRAR (Nome, Endereço, Telefone)\n2 - LISTAR REGISTRO\n3 - CONSULTAR O REGISTRO");
+            Console.Write("Escolha a opção (N°)...........:");
+            return;            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using BibliotecaCadastro;
+
+string[] nome = new string[100];
+string[] endereco = new string[100];
+string[] telefone = new string[100];
+int escolha = 1;
+int contador = 0;
+int Manter = 0;
+
+
+if (escolha == 1 || escolha == 2 || escolha == 3)
+{
+    while (Manter == 0)
+    {
+        Ferramentas.Menu();
+        escolha = Convert.ToInt16(Console.ReadLine());
+        switch (escolha)
+        {
+            case 1:
+                {
+                    for (int i = 0; i < nome.Length; i++)
+                    {
+                        Console.Write($"Informe o nome da {i + 1}° pessoa:");
+                        nome[i] = Console.ReadLine();
+                        Console.Write($"Informe o endereço da {i + 1}° pessoa:");
+                        endereco[i] = Console.ReadLine();
+                        Console.Write($"Informe  o telefone da {i + 1}° pessoa:");
+                        telefone[i] = Console.ReadLine();
+
+                        Console.WriteLine("Deseja continuar cadastrando? (0 -> Sim/1 -> Não)");
+                        Console.Write("Escolha a opção (N°)...........:");
+                        Manter = Convert.ToInt16(Console.ReadLine());
+                        if (Manter != 0)
+                        {
+                            Manter = 0;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            case 2:
+                {
+
+                    break;
+                }
+            case 3:
+                {
+
+                    break;
+                }
+            default:
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Opção invalida");
+                    Console.ReadKey();
+                    break;
+                }
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
